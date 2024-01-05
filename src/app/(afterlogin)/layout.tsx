6a6283d11e89ce406ffd3d8ec7e38abee79a0d42
minh0518 <rubics@naver.com>
2024-01-05 18:30:34 +0900

    chore: Tanstack/Query provider적용
    (afterlogin) 레이아웃에 적용해서 로그인 이후 적용 될 수 있도록 했습니다

---
+++ src/app/(afterlogin)/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from 'next';
+
+import RQProvider from './_components/RQProvider';
+
+import { ReactChildrenProps } from '@/app/types/common';
+
+export const metadata: Metadata = {
+  title: '홈',
+  description: 'HOME',
+};
+
+export default function AfterLoginLayout({ children }: ReactChildrenProps) {
+  return (
+    <div>
+      <RQProvider>
+        <div>{children}</div>
+      </RQProvider>
+    </div>
+  );
+}
