31e5a2f228aa792d329c85bf9bef248218c9f153
minh0518 <rubics@naver.com>
2024-05-15 12:50:55 +0000

    fix: 카카오 리다이렉션 URL 수정

--- src/config/path.ts
+++ src/config/path.ts
@@ -2,7 +2,7 @@ export const SOCIAL_ACCESS_URL = {
   KAKAO:
     process.env.NODE_ENV === 'development'
       ? `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_ROUTE_HANDLER}/accounts/login/process`
-      : `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL_PROD}/accounts/login/process`,
+      : `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_CLIENT_PROD_URL}/accounts/login/process`,
   NAVER: '',
   GOOGLE: '',
 };
